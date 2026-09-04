1565c7cdfd819cfcf7b861a3ffdbc8cf16e37ed8
kristee <kristeetannn@gmail.com>
2026-09-04 15:31:17 +0800

    Fun conditions

--- SIT/hello.py
+++ SIT/hello.py
@@ -23,11 +23,14 @@ followers -= 10
 print("Day 3:", followers)'''
 
 username = input("Enter username: ")
-age = input("Enter Age: ")
+age = int(input("Enter Age: "))
 category = input("Enter Content Category: ")
 
 print("\nInstagram Profile")
-print("===============================")
+print("===================")
 print("Username:", username)
 print("Age:", age)
 print("Category:", category)
+
+if age>40 and category == "fun":
+    print("You are old what is fun for you??") 
